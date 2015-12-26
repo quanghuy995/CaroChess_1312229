7169c14ab64d1d9d0637c93398a870fd34ee22a6
Bui Quang Huy <quanghuy995@gmail.com>
2015-12-26 22:06:31 +0700

    Set Attribute and Design MVVM
    cài đặt các phương thức get{}, set{}, thiết kế theo mô hình MVVM

---
+++ CaroChess_1312229/CaroChess_1312229/Models/Board.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CaroChess_1312229.Models
+{
+    class Board
+    {
+        private int _cellWidth = 40;
+        private int _cellHeight = 40;
+        private int _Row = 12;
+        private int _Col = 12;
+
+        public Cells[,] Cell { get; set; }
+        public enum Cells
+        {
+            None = 0,
+            Player1 = 1,
+            Player2 = 2
+        };
+
+        public Board()
+        {
+            Cell = new Cells[12, 12];
+            for (int i = 0; i < 12; i++)
+                for (int j = 0; j < 12; j++)
+                {
+                    Cell[i, j] = Cells.None;
+                }
+        }
+
+        public int cellWidth
+        {
+            set
+            {
+                _cellWidth = value;
+            }
+            get
+            {
+                return _cellWidth;
+            }
+        }
+
+        public int cellHeight
+        {
+            set
+            {
+                _cellHeight = value;
+            }
+            get
+            {
+                return _cellHeight;
+            }
+        }
+        public int Row
+        {
+            set
+            {
+                _Row = value;
+            }
+            get
+            {
+                return _Row;
+            }
+        }
+
+        public int Col
+        {
+            set
+            {
+                _Col = value;
+            }
+            get
+            {
+                return _Col;
+            }
+        }
+    }
+}
---
+++ CaroChess_1312229/CaroChess_1312229/ViewModels/BoardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaroChess_1312229.Models;
+
+namespace CaroChess_1312229.ViewModels
+{
+    class BoardViewModel
+    {
+        public Board CurrentBoard { get; set; }
+
+        public BoardViewModel()
+        {
+            CurrentBoard = new Board();
+        }
+    }
+}
